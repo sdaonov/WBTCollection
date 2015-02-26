a016d12af5c3d18b6f94fcb5f091d9deec014e78
sdaonov <sdaonov@gmail.com>
2015-02-26 15:21:32 -0600

    ok

--- NovLocator/NovLocator/NovLocator/Views/Phones/Main.xaml.cs
+++ NovLocator/NovLocator/NovLocator/Views/Phones/Main.xaml.cs
@@ -17,91 +17,91 @@ namespace NovLocator.Views.Phones
 
 
 
-    public partial class Main : ContentPage
-    {
+	public partial class Main : ContentPage
+	{
 
-        const int TIMEOUT = 100000;
+		const int TIMEOUT = 100000;
 
-        private ObservableCollection<Facility> _facilities = new ObservableCollection<Facility>();
+		private ObservableCollection<Facility> _facilities = new ObservableCollection<Facility>();
 
-        public Main()
-        {
+		public Main()
+		{
 
-            InitializeComponent();
+			InitializeComponent();
 
-            BindingContext = App.Locator.Main;
+			BindingContext = App.Locator.Main;
 
-            lstFacilities.ItemTapped += lstFacilities_ItemTapped;
+			lstFacilities.ItemTapped += lstFacilities_ItemTapped;
 
-            Refresh();
+			Refresh();
 
-        }
+		}
 
-        //private async void btnView_Clicked(object sender, EventArgs e)
-        //{
+		//private async void btnView_Clicked(object sender, EventArgs e)
+		//{
 
-        //}
+		//}
 
-        //private async void btnUncheckAll_Clicked(object sender, EventArgs e)
-        //{
+		//private async void btnUncheckAll_Clicked(object sender, EventArgs e)
+		//{
 
-        //}
+		//}
 
-        private async void lstFacilities_ItemTapped(object sender, ItemTappedEventArgs e)
-        {
+		private async void lstFacilities_ItemTapped(object sender, ItemTappedEventArgs e)
+		{
 
 			var facilityMap = App.Locator.FacilityMap;
 			facilityMap.Current = (Facility)e.Item;
 
-            await Navigation.PushAsync(new FacilityMap());
+			await Navigation.PushAsync(new FacilityMap());
 
-        }
+		}
 
-        private async void btnFacilityView_Clicked(object sender, EventArgs e)
-        {
-            await DisplayAlert("Alert", "You have been alerted", "OK");
-        }
+		private async void btnFacilityView_Clicked(object sender, EventArgs e)
+		{
+			await DisplayAlert("Alert", "You have been alerted", "OK");
+		}
 
-        private async void Refresh()
-        {
+		private async void Refresh()
+		{
 
-            var facilities = await FetchFacilities();
-            foreach(var fac in facilities) _facilities.Add(fac);
+			var facilities = await FetchFacilities();
+			foreach(var fac in facilities) _facilities.Add(fac);
 
-            lstFacilities.ItemsSource = _facilities;
+			lstFacilities.ItemsSource = _facilities;
 
-        }
+		}
 
-        // Gets weather data from the passed URL.  
-        private async Task<List<Facility>> FetchFacilities()
-        {
+		// Gets weather data from the passed URL.  
+		private async Task<List<Facility>> FetchFacilities()
+		{
 
-            var result = new List<Facility>();
-            var url = "https://go.nov.com/api/facility_locator/facility";
+			var result = new List<Facility>();
+			var url = "https://go.nov.com/api/facility_locator/facility";
 
-            var uriFacilities = new Uri(url);
-            var request = HttpWebRequest.Create(uriFacilities);
-            request.Method = "GET";
+			var uriFacilities = new Uri(url);
+			var request = HttpWebRequest.Create(uriFacilities);
+			request.Method = "GET";
 
-            using (var rsp = await request.GetResponseAsync(TIMEOUT))
-            {
-                using (var rst = rsp.GetResponseStream())
-                {
-                    using (var sr = new StreamReader(rst))
-                    {
+			using (var rsp = await request.GetResponseAsync(TIMEOUT))
+			{
+				using (var rst = rsp.GetResponseStream())
+				{
+					using (var sr = new StreamReader(rst))
+					{
 
-                        var jresult = sr.ReadToEnd();
-                        result = JsonConvert.DeserializeObject<List<Facility>>(jresult);
+						var jresult = sr.ReadToEnd();
+						result = JsonConvert.DeserializeObject<List<Facility>>(jresult);
 
-                    }
-                }
-            }
+					}
+				}
+			}
 
-            return result;
+			return result;
 
-        }
+		}
 
 
-    }
+	}
 
 }
